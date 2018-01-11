48c44e285e8927b3a7f98dec72e707295531778b
Ugosylva Blog <nugosylva7@gmail.com>
2018-01-11 16:56:04 +0100

    new project

---
+++ app/models/price.rb
@@ -0,0 +1,10 @@
+class Price < ApplicationRecord
+
+def self.get_data
+	blockchain_url = 'https://blockchain.info/ticker'
+	btc_data = JSON.parse(RestClient.get(blockchain_url).body)
+	price_data = btc_data['USD']['buy']
+	price = Price.new(price: price_data)
+	price.save
+end
+end
